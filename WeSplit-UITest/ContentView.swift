e881b01af121d03f98266841330cb45e5071b305
Liliia Korablova <lilyakorablova@gmail.com>
2020-11-17 19:38:47 +0100

    Initial Commit

---
+++ WeSplit-UITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit-UITest
+//
+//  Created by Viktor Nakyden on 17.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit-UITest/WeSplit_UITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_UITestApp.swift
+//  WeSplit-UITest
+//
+//  Created by Viktor Nakyden on 17.11.20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_UITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
